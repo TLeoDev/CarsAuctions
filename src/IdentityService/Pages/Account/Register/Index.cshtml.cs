1d2880d94d46cafc8a005d31152069d7fdf40de1
tleodev <leo.tuaillon@etu.uca.fr>
2025-03-10 13:50:14 +0100

    🧑‍💻 Register - Login page customization + add the form

--- src/IdentityService/Pages/Account/Register/Index.cshtml.cs
+++ src/IdentityService/Pages/Account/Register/Index.cshtml.cs
@@ -6,57 +6,62 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace IdentityService.Pages.Register;
-
-[SecurityHeaders]
-[AllowAnonymous]
-public class Index : PageModel
+namespace IdentityService.Pages.Register
 {
-    private readonly UserManager<ApplicationUser> _userManager;
-    public Index(UserManager<ApplicationUser> userManager)
+    [SecurityHeaders]
+    [AllowAnonymous]
+    public class Index : PageModel
     {
-        _userManager = userManager;
-    }
-    
-    [BindProperty]
-    public RegisterViewModel Input { get; set; }
-    
-    [BindProperty]
-    public bool RegisterSuccess { get; set; }
-    public IActionResult OnGet(string returUrl)
-    {
-        Input = new RegisterViewModel
+        private readonly UserManager<ApplicationUser> _userManager;
+ 
+        public Index(UserManager<ApplicationUser> userManager)
         {
-            ReturnUrl = returUrl
-        };
-
-        return Page();
-    }
-
-    public async Task<IActionResult> OnPost()
-    {
-        if (Input.Button != "register") return Redirect("~/");
-
-        if (ModelState.IsValid)
-        {
-            var user = new ApplicationUser
-            {
-                UserName = Input.Username,
-                Email = Input.Email,
-                EmailConfirmed = true
-            };
-            
-            var result = await _userManager.CreateAsync(user, Input.Password);
-            
-            if (result.Succeeded)
-            {
-                await _userManager.AddClaimsAsync(user, new Claim[]
-                {
-                    new Claim(JwtClaimTypes.Name, Input.FullName)
-                });
-                RegisterSuccess = true;
-            }
+            _userManager = userManager;
         }
-        return Page();
+ 
+        [BindProperty]
+        public RegisterViewModel Input { get; set; }
+ 
+        [BindProperty]
+        public bool RegisterSuccess { get; set; }
+ 
+        public IActionResult OnGet(string returnUrl)
+        {
+            Input = new RegisterViewModel
+            {
+                ReturnUrl = returnUrl,
+            };
+ 
+            return Page();
+        }
+ 
+        public async Task<IActionResult> OnPost()
+        {
+            if (Input.Button != "register") return Redirect("~/");
+ 
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser
+                {
+                    UserName = Input.Username,
+                    Email = Input.Email,
+                    EmailConfirmed = true
+                };
+ 
+                var result = await _userManager.CreateAsync(user, Input.Password);
+ 
+                if (result.Succeeded)
+                {
+                    await _userManager.AddClaimsAsync(user, new Claim[]
+                    {
+                        new Claim(JwtClaimTypes.Name, Input.FullName)
+                    });
+ 
+                    RegisterSuccess = true;
+                }
+            }
+ 
+            return Page();
+        }
     }
 }
